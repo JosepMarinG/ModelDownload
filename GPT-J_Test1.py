fe3f11a51836c0fd0364cdbb8927ef08d35da8d4
JosepMarinG <123630716+JosepMarinG@users.noreply.github.com>
2025-11-12 23:51:14 +0100

    Limpieza de test scripts

--- GPT-J_Test1.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-model_name = "EleutherAI/gpt-j-6B"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
--- PytorchTest1.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    print("CUDA está disponible. Se usará la GPU.")
-else:
-    print("CUDA NO está disponible. Se usará la CPU.")
